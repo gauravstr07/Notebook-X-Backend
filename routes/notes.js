d2c749fc82261f5775c6173ab7dff908d66a8948
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-14 01:45:47 +0530

    Update an existing note

--- routes/notes.js
+++ routes/notes.js
@@ -51,4 +51,35 @@ router.post(
   }
 );
 
+
+// 3. Update an existing note
+router.put(
+  "/updatenote/:id",
+  fetchUser,
+  async (req, res) => {
+    const { title, description, tag } = req.body
+    // Create a new note update
+    const newNote = {}
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    // Find a note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if (!note) { res.status(404).send("Not Found") }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed😏")
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json({ note })
+
+  }
+)
 module.exports = router;
