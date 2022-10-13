a10f744f95d75b0568ba595fbfcfdf50ed2ab5ab
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-13 17:32:57 +0530

    Notebook-X conneted to server and database

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const connectToMongo = require("./db");
 
+connectToMongo();
+
+const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to server');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}📡`)
-})     
+  console.log(`Server running on port ${port}📡`);
+});
+
+//
