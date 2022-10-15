f7e505537b9d18aae09076b32a05c95d8109e979
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-15 21:45:16 +0530

    Bugs Fixed

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,10 @@ router.post(
     }),
   ],
   async (req, res) => {
+    let success = false
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     try {
       // Check wheather the user is already exits
@@ -28,7 +29,7 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ error: "Sorry user already exits with this email" });
+          .json({success, error: "Sorry user already exits with this email" });
       }
       const salt = await bcrypt.genSalt(10);
       const secPassword = await bcrypt.hash(req.body.password, salt);
@@ -45,7 +46,8 @@ router.post(
         },
       };
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json({ authToken });
+      success = true
+      res.json({success, authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occured");
