38c73eef776598f0d753c25dad891b9a9e999e93
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-15 15:55:20 +0530

    npm i cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectToMongo = require("./db");
+const cors = require('cors');
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
